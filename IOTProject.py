96849401326386bd9dae671ba0bfeffe9fb18c13
Ben Howarth <benjamin.howarth@sjsu.edu>
2020-11-27 20:54:40 -0800

    cleaned up code

--- IOTProject.py
+++ IOTProject.py
@@ -6,11 +6,14 @@ import neopixel
  
 pixel_pin = board.D18
  
-num_pixels = 30
+num_pixels = 100
  
-ORDER = neopixel.GRB
- 
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness = 0.2, auto_write = False, pixel_order = ORDER)
+ORDER = "BRG"
+
+enabled = True
+color = 255
+
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness = 1.0, auto_write = False, pixel_order = ORDER)
  
 def wheel(pos):
     if pos < 0 or pos > 255:
@@ -30,30 +33,17 @@ def wheel(pos):
         g = int(pos * 3)
         b = int(255 - pos * 3)
     return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
- 
-def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(pixel_index & 255)
-            pixels.show()
-            time.sleep(wait)
 
 while True:
-    pixels.fill((255, 0, 0))
+    if enabled == True:
+        pixels.fill((255, 255, 255))
+    
+    else:
+        pixels.fill((0, 0, 0))
+        
     pixels.show()
     time.sleep(1)
  
-    pixels.fill((0, 255, 0))
-    pixels.show()
-    time.sleep(1)
- 
-    pixels.fill((0, 0, 255))
-    pixels.show()
-    time.sleep(1)
- 
-    rainbow_cycle(0.001)
-
 try:
     connection = SerialManager()
     a = ArduinoApi(connection = connection)
